6a6077f7d6075dbdda31738c2bd26450f483a8be
RodAcevedoF <raacevedof@gmail.com>
2025-01-30 16:23:40 +0100

    db config

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,38 @@
-import pg from "pg";
+/* import pg from "pg";
 import config from "./config.js";
 
 const { Pool } = pg;
 
 const pool = new Pool(config.db);
+
+export default {
+  query: (text, params) => pool.query(text, params),
+  connect: () => pool.connect()
+};
+ */
+
+import pg from "pg";
+import config from "./config.js";
+
+const { Pool } = pg;
+
+let poolConfig;
+
+if (config.db) {
+  const dbUrl = new URL(config.db);
+  poolConfig = {
+    user: dbUrl.username,
+    host: dbUrl.hostname,
+    database: dbUrl.pathname.replace("/", ""),
+    password: dbUrl.password,
+    port: dbUrl.port || 5432,
+    ssl: dbUrl.searchParams.get("sslmode") === "require" ? { rejectUnauthorized: false } : false,
+  };
+} else {
+  throw new Error("DATABASE_URL is missing in the environment variables!");
+}
+
+const pool = new Pool(poolConfig);
 
 export default {
   query: (text, params) => pool.query(text, params),
