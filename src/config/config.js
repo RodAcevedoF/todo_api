7e3165274dab2341c01dd4b4060598b28f4ce5c8
RodAcevedoF <raacevedof@gmail.com>
2025-01-30 16:11:26 +0100

    config databaseURL

--- src/config/config.js
+++ src/config/config.js
@@ -4,13 +4,7 @@ dotenv.config();
 
 const config = {
   port: process.env.PORT || 4000,
-  db: {
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: Number(process.env.DB_PORT)
-  },
+  db: process.env.DATABASE_URL || "",
   jwt: {
     secret: process.env.JWT_SECRET,
     expiresIn: process.env.JWT_EXPIRES || "1d"
@@ -19,3 +13,11 @@ const config = {
 };
 
 export default config;
+
+/* db: {
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT)
+  } */
