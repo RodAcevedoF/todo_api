52f9e00cbb19df74716300bd996ebaa0ee4cdd5f
RodAcevedoF <raacevedof@gmail.com>
2025-02-07 19:58:45 +0100

    ratelimit change

--- src/middlewares/rateLimit.js
+++ src/middlewares/rateLimit.js
@@ -1,19 +1,19 @@
 import rateLimit from "express-rate-limit";
 
 export const loginRateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 5,
+  windowMs: 5 * 60 * 1000,
+  max: 12,
   message: {
     success: false,
-    error: "Too many login attempts. Please try again after 15 minutes."
+    error: "Too many login attempts. Please try again after 5 minutes."
   }
 });
 
 export const registerRateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 5,
+  windowMs: 5 * 60 * 1000,
+  max: 12,
   message: {
     success: false,
-    error: "Too many registration attempts. Please try again after 15 minutes."
+    error: "Too many registration attempts. Please try again after 5 minutes."
   }
 });
