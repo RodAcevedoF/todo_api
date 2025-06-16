cc7399f1f0e9d87eb3b7812d36ded413c65a817a
RodAcevedoF <raacevedof@gmail.com>
2025-06-16 17:33:30 +0200

    feat: adding vercel domain

--- src/app.js
+++ src/app.js
@@ -25,7 +25,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5175",
   "http://localhost:5174",
-  "https://service.todo-api.site"
+  "https://service.todo-api.site",
+  "getdoneapp.vercel.app"
 ];
 
 app.use(
